15820060d4233b3d69a2a82e2014a3d88b6dd08d
VISHAL MAURYA <vishumaurya0444@gmail.com>
2022-09-07 22:06:09 +0530

    Add files via upload
    admin

---
+++ NotepadEditor/src/notepadeditor/NotepadEditor.java
@@ -0,0 +1,18 @@
+
+package notepadeditor;
+
+/**
+ *
+ * @author VISHAL MAURYA
+ */
+public class NotepadEditor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Notepad();
+    }
+    
+}
